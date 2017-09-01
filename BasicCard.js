51ac7af3095ed01345a6d23275c9a1fb2139fb1a
Kara Peterson <karapeterson@Karas-MacBook.local>
2017-09-01 11:23:48 -0600

    first commit

---
+++ BasicCard.js
@@ -0,0 +1,9 @@
+//Basic card constructor
+
+function BasicCard(front, back) {
+		this.front = front;
+		this.back = back;
+
+}
+
+module.exports = BasicCard;
---
+++ ClozeCard.js
@@ -0,0 +1,10 @@
+//Cloze card constructor
+
+function ClozeCard (text, cloze) {
+		this.text = text;
+		this.cloze = cloze;
+
+}
+
+
+module.exports = ClozeCard;
